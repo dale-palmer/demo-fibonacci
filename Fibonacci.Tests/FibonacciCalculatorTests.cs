4ab2e7c1271c463de65914f745d9634807de76a8
Dale Palmer <dalepalmer@outlook.com>
2021-08-26 10:19:46 +0100

    Added calculator.

---
+++ Fibonacci.Tests/FibonacciCalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Fibonacci.Tests
+{
+    class FibonacciCalculatorTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(4, 3)]
+        [TestCase(5, 5)]
+        [TestCase(6, 8)]
+        public void FibonacciCalculator_Calculate_WithTerm_ReturnsValue(int term, int expected)
+        {
+            var actual = FibonacciCalculator.Calculate(term);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Fibonacci/FibonacciCalculator.cs
@@ -0,0 +1,8 @@
+﻿namespace Fibonacci
+{
+    public static class FibonacciCalculator
+    {
+        public static int Calculate(int n) =>
+            n < 2 ? n : Calculate(n - 1) + Calculate(n - 2);
+    }
+}
